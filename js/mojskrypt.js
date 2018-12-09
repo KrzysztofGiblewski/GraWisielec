52bf2759c372b05d4596018c3caad5334eef16c7
Krzysztof Giblewski <krzysztofgiblewski@gmail.com>
2018-12-09 11:51:47 +0100

    Po wyodrebnieniu skryptu do osobnego pliku mojskrypt.js

--- js/mojskrypt.js
+++ js/mojskrypt.js
@@ -1,49 +1,77 @@
-   
-function wowow(){
-     alert("WOWOWOWWWW!");
-}
-        var zycia;
-        var pobraneDane;
-        var ileLiter;
-        var pusteSlowo;
-        var wylosowaneSlowo;
-        var bazaSlow;
-        //wylosowaneSlowo; pusteSlowo, bazaSlow
+var zycia;
+var wylosowaneSlowo;
+var pusteSlowo = [""];
+var pomocnicze = "";
 
-        function losuj(wylosowaneSlowo, pusteSlowo, bazaSlow, wylosowanyIndex) {
-            bazaSlow["zamek", "drzwi", "stolik", "rekin", "budynek"];
+function losuj() {
 
             alert('NOWA GRA!');
+            pusteSlowo = [""];
+            zycia = 9;
+
+            var bazaSlow = ["ziomek", "oko", "mama"
+                 ,"zamek", "drzwi", "stolik", "rekin", "budynek",
+                 "komputer", "drzewo", "czajnik", "samolot", "miska", "kurczak",
+                 "filmy", "herbata", "ogień", "niebo", "okulary",
+                  "jogurt", "jabłko", "cukier", "czosnek"
+            ];
+
 
 
             var wylosowanyIndex = Math.floor(Math.random() * bazaSlow.length);
             wylosowaneSlowo = bazaSlow[wylosowanyIndex];
             wylosowaneSlowo = wylosowaneSlowo.toUpperCase();
 
-            for (var i = 0;  wylosowaneSlowo.length>i; i++) {
-                pusteSlowo.push("_");
+            for (var i = 0; wylosowaneSlowo.length > i; i++) {
+                pusteSlowo[i] = "-";
 
             }
-            document.getElementById("pusteSlowo").innerText = wylosowaneSlowo;
+            document.getElementById("paragraf2").innerText = "słowo składa się z " + pusteSlowo.length + " liter";
+            document.getElementById("pusteSlowoText").innerText = pusteSlowo.join("");
+
+
         }
 
 
         function sprawdz() {
 
-            var a = document.forms['formularz'].elements['a'].value;
-
-            var pobraneDane = document.getElementsByName("input").value;
+            var pobraneDane;
+            var traf = false;
+            pobraneDane = document.getElementById("LitLubSlo").value;
             pobraneDane = pobraneDane.toUpperCase();
-            for (var i = 0; i < pobraneDane.length; i++) {
-                for (var i = 0; i < wylosowaneSlowo.length; i++) {
-                    if (pobraneDane[i] == wylosowaneSlowo[i]) {
-                        pusteSlowo[i] = pobraneDane[i];
-                    }
+
+            if (pobraneDane.length > 1) {
+                if (pobraneDane == wylosowaneSlowo) {
+                        document.getElementById("paragraf").innerText = "BRAWO ZGADŁEŚ";
+                    document.getElementById("pusteSlowoText").innerText = wylosowaneSlowo;
+
+                    traf = true;
                 }
             }
-            return pusteSlowo;
-            document.getElementById("pusteSlowo").innerHTML = pusteSlowo;
-            if (wylosowaneSlowo == pusteSlowo){
-                do.getElementById("#komentarz").innerText = "WYGRAŁEŚ";
+            if (pobraneDane.length = 1) {
+                for (i = 0; wylosowaneSlowo.length > i; i++) {
+                    if (wylosowaneSlowo[i] == pobraneDane) {
+                        pusteSlowo[i] = pobraneDane;
+                        document.getElementById("paragraf").innerText = "Trafiłeś " + pobraneDane + " pasuje";
+                        document.getElementById("pusteSlowoText").innerText = pusteSlowo.join("");
+
+                        traf = true;
+                    } else
+                        pusteSlowo[i] = pusteSlowo[i];
+
+                }
+                if (pusteSlowo.join("") == wylosowaneSlowo)
+                        document.getElementById("paragraf").innerText = "BRAWO ZGADŁEŚ";
             }
+            if (traf == false) {
+                zycia--;
+                document.getElementById("paragraf").innerText = "NIE trafiłeś " +
+                    pobraneDane + "  nie znajduje się w szukanym słowie pozostało ci " + zycia + " podejść";
+
+
+            }
+
+
+
         }
+
